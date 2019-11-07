aad871ef5f8bb9b858508952a44101eb0892351c
César Vargas Escorcia <thecesarvargas@gmail.com>
2019-11-07 08:55:55 -0600

    Operadores aritmeticos cortos

--- src/main/java/com/practicascesar/proyecto1/OperadoresAritmeticos.java
+++ src/main/java/com/practicascesar/proyecto1/OperadoresAritmeticos.java
@@ -11,28 +11,19 @@ public class OperadoresAritmeticos {
         Scanner entrada = new Scanner(System.in);
         
         
-        float num1, num2, suma, resta, division, multiplicacion, residuo;
+        float prueba = 5f;
         
-        System.out.println("Ingresa 2 numeros: \t\n");
+        System.out.println("Valor original de prueba: " + prueba);
+        prueba = prueba + 1;
+        System.out.println("Res prueba = prueba + 1... " + prueba);
+        prueba+=1;
+        System.out.println("Res prueba+=1... " + prueba);
+        prueba++;
+        System.out.println("Res prueba++... " + prueba);
+        ++prueba;
+        System.out.println("Res ++prueba... " + prueba);
         
-        System.out.println("Ingresa primer numero...");
-        num1 = entrada.nextFloat();
+        /*Lo mismo pasa con lo soperadores de resta, division, multiplicacion*/
         
-        System.out.println("Ingresa segundo numero...");
-        num2 = entrada.nextFloat();
-        
-        
-        suma = num1 + num2;
-        resta = num1 - num2;
-        division = num1 / num2;
-        multiplicacion = num1 * num2;
-        residuo = num1 % num2;
-        
-        System.out.println("Resultados.....");
-        System.out.println("Suma: "+ suma);
-        System.out.println("Resta: "+ resta);
-        System.out.println("Multiplicación: "+ multiplicacion);
-        System.out.println("División: " +division);
-        System.out.println("Residuo: "+ residuo);
     }
 }
