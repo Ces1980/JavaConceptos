788cb31340d67eb5f9e52d7ac78a2ec822a775c1
César Vargas Escorcia <thecesarvargas@gmail.com>
2019-11-13 22:12:18 -0600

    Azar con While --> encuentra el número

---
+++ src/main/java/com/practicascesar/proyecto1/AdivinarNumero.java
@@ -0,0 +1,37 @@
+package com.practicascesar.proyecto1;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class AdivinarNumero {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        /*Generando el numero aleatorio del programa*/
+        int aleatorio = rand.nextInt(5) + 1;
+        /*Conteos del ciclo while*/
+        int conteos = 1;
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Humano que numero estoy pensado (entre 1 y 5) dijita 0 para salir"));
+        while (num != aleatorio) {
+            num = Integer.parseInt(JOptionPane.showInputDialog("Humano estupdo te equivocaste en que numero estoy pensado (entre 1 y 5) dijita 0 para salir"));
+            if (num == 0) {
+                break;
+            }
+            aleatorio = rand.nextInt(5) + 1;
+
+            conteos++;
+        }
+
+        /*Mensajes de salida al empatar numeros*/
+        if (num != 0) {
+            JOptionPane.showMessageDialog(null, "Bien jugado humano");
+        } else {
+            JOptionPane.showMessageDialog(null, "Huamno estupido el numero era " + aleatorio);
+        }
+
+        JOptionPane.showMessageDialog(null, "Numero de iteracciones..." + conteos);
+    }
+
+}
